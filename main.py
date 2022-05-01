853344e4ddcb3eb65dd79d44290b36a31d0265f3
SPT <arthit0ak@gmail.com>
2022-05-01 21:16:29 +0700

    commit

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from database import dataX,dataY,dataZ,dataSpectrumX,dataSpectrumY,dataSpectrumZ
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 dataX = dataX.tolist()
 dataY = dataY.tolist()
